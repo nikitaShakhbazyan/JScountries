7185c685624dcf3ca0d083125186c8b6810cd1e7
nickshakh <shakhbazyann@gmail.com>
2023-10-12 01:16:27 +0400

    Made some changes
    Corrected searching input,added styles.
    Added sources

--- scripts/script.js
+++ scripts/script.js
@@ -3,78 +3,99 @@ const searchInput = document.getElementById('searchInput');
 const searchResults = document.getElementById('searchResults');
 
 const container = document.querySelector('.container');
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const countries = data.map((country) => ({
-      name: country.name,
-      flag: country.flags.png,
-      neighbors: country.borders,
-      neighborsFlag :country.flags,
-      area : country.area,
-      population : country.population,
-    }));
+const countryDivs = []; 
 
-    countries.forEach((country) => {
-      const countryDiv = document.createElement('div');
-      countryDiv.classList.add('text-light','text-center','bg-dark','align-items-center','row')
-      countryDiv.style.border = '2px solid gold'
-      const img = document.createElement('img','p-2');
-      img.classList.add('col');
-      img.src = country.flag;
+const fetchFunction = function() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const countries = data.map((country) => ({
+        name: country.name,
+        flag: country.flags.png,
+        neighbors: country.borders,
+        neighborsFlag: country.flags,
+        area: country.area,
+        population: country.population,
+      }));
 
-      const h2 = document.createElement('h2');
-      h2.classList.add('col');
-      h2.textContent = country.name;
+      countries.forEach((country) => {
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add(
+          'countryContainer',
+          'text-light',
+          'text-center',
+          'bg-dark',
+          'align-items-center',
+          'row'
+        );
+        countryDiv.style.border = '2px solid gold';
+        countryDivs.push(countryDiv); 
 
-      const h4 = document.createElement('h4')
-      h4.classList.add('col-md-6',)
-      h4.textContent = `Country's area is ${country.area / 1000} thousand square km.`
+        const img = document.createElement('img');
+        img.classList.add('col', 'p-2');
+        img.src = country.flag;
 
-      const h5 = document.createElement('h5')
-      h5.classList.add('col-md-6')
-      h5.textContent = `The country's population is approximately ${Math.round(country.population/1000000)} millions `
-      
+        const h2 = document.createElement('h2');
+        h2.classList.add('col');
+        h2.textContent = country.name;
 
-      const p = document.createElement('p')
-      p.classList.add('col','text-danger')
-      p.style.display = 'block'
-      p.style.fontSize = '22px'
-      p.innerHTML = `<bold>${country.neighbors}</bold>`
+        const h4 = document.createElement('h4');
+        h4.classList.add('col-md-6');
+        h4.textContent = `Country's area is ${country.area / 1000} thousand square km.`;
 
-      if (country.neighbors === undefined) {
-        p.textContent = 'There are no neighboring countries';
-      } else {
-        p.textContent = `Neighbors: ${country.neighbors}`;
-      }
+        const h5 = document.createElement('h5');
+        h5.classList.add('col-md-6');
+        h5.textContent = `The country's population is approximately ${country.population.toLocaleString()} people`;
 
-      container.appendChild(countryDiv);
-      countryDiv.appendChild(img);
-      countryDiv.appendChild(h2);
-      countryDiv.appendChild(h5)
-      countryDiv.appendChild(h4)
-      countryDiv.appendChild(p)
+        const p = document.createElement('p');
+        p.classList.add('col', 'text-danger');
+        p.style.display = 'block';
+        p.style.fontSize = '22px';
+        if (country.neighbors === undefined) {
+          p.textContent = 'There are no neighboring countries';
+        } else {
+          p.textContent = `Neighbors: ${country.neighbors}`;
+        }
 
-    });
+        // const a = document.createElement('a');
+        // a.classList.add('col-md-2', 'alert', 'alert-success', 'aTagForMore');
+        // a.textContent = `Click here for more information`;
+        // a.href = `country_details.html?country=${country.name}`;
 
-    const dataNamesForSearching = data.map((el) => {
-        return el.flag
-    });
+        const containerForData = document.createElement('div');
+        containerForData.classList.add('dataContainer');
+        container.appendChild(countryDiv);
+        countryDiv.appendChild(img);
+        countryDiv.appendChild(h2);
+        countryDiv.appendChild(h5);
+        countryDiv.appendChild(h4);
+        countryDiv.appendChild(p);
+        // countryDiv.appendChild(a);
+      });
 
-    function searching () {
+      function searching() {
         const searchTerm = searchInput.value.toLowerCase();
         searchResults.innerHTML = '';
 
-    const filteredItems = Array.from(dataNamesForSearching).filter(item => {
-        const itemText = item.toLowerCase();
-        return itemText.includes(searchTerm);
-    })
-    filteredItems.forEach(item => {
-        const resultItem = document.createElement('li');
-        resultItem.textContent = item;
-        searchResults.appendChild(resultItem);
-    });
-    }
-           searchInput.addEventListener('input', searching);
-  });
+        countryDivs.forEach((countryDiv) => {
+          const itemText = countryDiv.textContent.toLowerCase();
+          if (itemText.includes(searchTerm)) {
+            container.appendChild(countryDiv);
+          } else {
+            container.removeChild(countryDiv);
+          }
+        });
+      }
+      searchInput.addEventListener('input', searching);
 
+      function ClearingResults() {
+        const query = searchInput.value;
+        if (query === '') {
+          searchResults.innerHTML = '';
+        }
+      }
+      searchInput.addEventListener('input', ClearingResults);
+    });
+};
+
+fetchFunction();
